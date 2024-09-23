80721523ff1287db3b4f7d42eff6bfc524b710e5
Mariam (Mëmø) <33464259+Mariam-Amr@users.noreply.github.com>
2024-09-23 10:30:41 +0300

    Create Dut.py

---
+++ scr/Dut.py
@@ -0,0 +1,27 @@
+"""
+This file contains the code logic and the DUT class
+"""
+class DUT:
+    def __init__(self):
+        self.state = {}
+
+
+    def set(self, key : str, value: int) -> bool:
+        if value < 0:
+            raise ValueError("Value cannot be negative.")
+        self.state[key] = value
+        return True
+
+
+    def get(self, key: str) -> int:
+        return self.state.get(key, 0)
+
+
+def choose_action(pv_production: int , house_consumption: int) -> str:
+    if pv_production > house_consumption :
+        return "Charge with Surplus"
+    elif pv_production < house_consumption:
+        return "Storge will supply house with power"
+    else:
+        return "Nothing"
+      
